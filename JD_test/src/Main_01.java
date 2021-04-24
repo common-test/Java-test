cda09a8c188d9d81431c0dfeb41304b71492e259
alex-wzc <moonwang1997@gmail.com>
2021-04-24 12:13:08 +0800

    first_commit_2021.4.24

--- JD_test/src/Main_01.java
+++ JD_test/src/Main_01.java
@@ -1,5 +1,7 @@
 public class Main_01 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("git_test");
+        System.out.println("git_test2");
     }
 }
